9fd7dfd4a5f2fac1064de3446cab1582e593f494
Rishi Patel <39495502+rishix23@users.noreply.github.com>
2020-12-09 03:00:23 +0000

    Update cwImproved.R

--- cwImproved.R
+++ cwImproved.R
@@ -1,20 +1,76 @@
-myLibraries<-c("mltools", "data.table", "caret", "dplyr", "corrplot", "memisc", "caret", "plyr")
+# ************************************************
+# PRACTICAL BUSINESS ANALYTICS
+# COM3018/COMM053
+#
+# Rishi Patel
+#
+# 16 NOVEMEMBER 2020
+#
+# ************************************************
+# R Script For Pre-processing Data
+
+# clears all objects in "global environment"
+rm(list=ls())
+
+# * ***********************************************
+# Global Environment Variables (available to all functions)
+
+
+# Define and then load the libraries used in this project
+# Library from CRAN     
+# mltools                    
+# data.table                     
+# caret                   
+# dplyr                    
+# corrplot               
+# memisc                
+# plyr 
+# pROC
+# rattle
+# Assertr
+
+myLibraries<-c("mltools", 
+               "data.table", 
+               "caret", 
+               "dplyr", 
+               "corrplot", 
+               "memisc", 
+               "plyr")
+
 library(pacman)
 pacman::p_load(char=myLibraries,install=TRUE,character.only=TRUE)
 
 # Functions Below
 
+# ************************************************
+# conversion() :
+#
+# converts fields with values of "no internet service" to "No"
+#
+# INPUT : 
+#            Object       - telco               - Telco dataset
+#
+# OUTPUT :
+#            List         - data frame          - telco data frame
+# ************************************************
 conversion <- function(telco) {
   telcoNoInternet <- which(telco$InternetService == "No")
   telco[telcoNoInternet,c(10,11,12,13,14,15)]<-"No"
   
   return (telco)
-  
 }
 
-
-# ********************************************************************************
+# ************************************************
+# detectOutlier() :
+#
 # Detecting outlier function using IQR
+#
+# INPUT : 
+#            Object       - x        - value within given field
+#
+# OUTPUT :
+#            List         - x        - Confusion matrix
+# ************************************************
 detectOutlier<-function(x) {
   Q1<- quantile(x,probs =.25)
   Q3<-quantile(x, probs =.75)
@@ -25,7 +81,19 @@ detectOutlier<-function(x) {
   
   x > upper_limit | x < lower_limit
 }
-# ********************************************************************************
+
+# ************************************************
+# removeOutliers() :
+#
+# loop through the Telco data dictionary columns and apply the detectOutlier function to each column
+#
+# INPUT : 
+#            Object       - data        - Telco dataset
+#            Object       - cols        - name of columns
+#
+# OUTPUT :
+#            List         - data        - return telco data frame with outliers removed
+# ************************************************
 # Removing outliers function
 removeOutliers<-function(data, cols = names(data)) {
   for(col in cols) {
@@ -33,19 +101,33 @@ removeOutliers<-function(data, cols = names(data)) {
   }
   return(data)
 }
-# ********************************************************************************
+
+# ************************************************
+# normalise() :
 # Normalising the data record
+#
+# INPUT : 
+#            Object       - x        - value to be normalised
+#
+# OUTPUT :
+#            Object         - x        - normalised value
+# ************************************************
 normalise<-function(x){
   x<-(x-min(x))/(max(x)-min(x))
   return(x)
 }
-# ********************************************************************************
-# Normalising all the data (not being used atm)
-# normaliseEverything<-function(data) {
-#   data<-sapply(data, normalise)
-#   return(data)
-# }
-# ********************************************************************************
+
+# ************************************************
+# getCorrelation() :
+#
+# compares each feild to each other and prints the correlation
+#
+# INPUT : 
+#            Object       - telco               - Telco dataset
+#
+# OUTPUT :
+#            Prints names to console
+# ************************************************
 # Correlation Function
 getCorrelation <- function(telco) { 
   for(i in 1:ncol(telco)){
@@ -60,21 +142,28 @@ getCorrelation <- function(telco) {
             if((correlation$estimate[[1]]) >= 0.4){
               print(paste(names(telco)[i],"-----",names(telco)[x]))
             }}}}}}}
-# ********************************************************************************
 
+# ********************************************************************************
+# PreProcessing() :
+#
+# entry point to execute the pre-processing
+#
+# INPUT: None
+#
+# OUTPUT: None
+# ************************************************
+PreProcessing <- function(){
 
 # Reading Telco
 telco<-read.csv("TelcoCustomerChurn.csv")
 
+#BASIC CLEANING
 # Getting rid of customerID
 telco<-telco[c(2:21)]
 
+#stores data types of each field in a dataframe
 numericOrSymbolic <- as.data.frame(sapply(telco, typeof))
 
-# Can use this code to get rid of NA values as opposed to the code below..
-# telco<-telco[complete.cases(telco),]
-
-#BASIC CLEANING
 #see what type of data each column is
 str(telco)
 
@@ -91,19 +180,12 @@ print(paste("% of data which is na's",(nrow(telcoNAs)/nrow(telco))*100,"%"))
 #remove the rows as such a small percentage
 telco <- na.omit(telco)
 
-#Removes outliers in the dataset
-telco<- removeOutliers(telco, cols = names(telco))
-
 # Standardisation (z-score normalisation)
-# z-scale
+# z-scale applying to MonthlyCharges and TotalCharges
 telco$MonthlyCharges<-as.data.frame(scale(telco$MonthlyCharges,center=TRUE, scale=TRUE))
 telco$TotalCharges<-as.data.frame(scale(telco$TotalCharges,center=TRUE, scale=TRUE))
 
-# # Normalising continuous ordinals
-# telco$MonthlyCharges<-normaliseEverything(telco$MonthlyCharges)
-# telco$TotalCharges<-normaliseEverything(telco$TotalCharges)
-
-# Same as above (although when using this you don't need normaliseEverything Function) Which one to use???
+# applying normalise function to MonthlyCharges and TotalCharges
 telco$MonthlyCharges<-sapply(telco$MonthlyCharges, normalise)
 telco$TotalCharges<-sapply(telco$TotalCharges, normalise)
 
@@ -113,16 +195,16 @@ telco[telcoNoInternet,c(9,10,11,12,13,14)]<-"No"
 telcoNoInternet <- which(telco$PhoneService == "No")
 telco[telcoNoInternet,7]<-"No"
 
-# # One hot encoding
+# One hot encoding
 dmy <- dummyVars(" ~ .", data = telco)
 telco <- data.frame(predict(dmy, newdata = telco))
 
-#Removing unecessary columns
-telco<-subset(telco, select=-c(genderFemale))#no impact on churn rates
+# Removing unecessary columns
+telco<-subset(telco, select=-c(genderFemale)) #no impact on churn rates
 telco<-subset(telco, select=-c(PartnerNo))
 telco<-subset(telco, select=-c(DependentsNo))
-telco<-subset(telco, select=-c(PhoneServiceNo))#no impact on churn rates 
-telco<-subset(telco, select=-c(PhoneServiceYes))#no impact on churn rates
+telco<-subset(telco, select=-c(PhoneServiceNo)) #no impact on churn rates 
+telco<-subset(telco, select=-c(PhoneServiceYes)) #no impact on churn rates
 telco<-subset(telco, select=-c(MultipleLinesNo))
 telco<-subset(telco, select=-c(OnlineBackupNo))
 telco<-subset(telco, select=-c(DeviceProtectionNo))
@@ -131,28 +213,17 @@ telco<-subset(telco, select=-c(StreamingTVNo))
 telco<-subset(telco, select=-c(StreamingMoviesNo))
 telco<-subset(telco, select=-c(PaperlessBillingNo))
 telco<-subset(telco, select=-c(ChurnNo))
-telco<-subset(telco, select=-c(genderMale))#no impact on churn rates
+telco<-subset(telco, select=-c(genderMale)) #no impact on churn rates
 telco<-subset(telco, select=-c(MultipleLinesYes))
-# ##############################################################################
-# 
-# telco<-subset(telco, select=-c(PartnerYes))
-# telco<-subset(telco, select=-c(DependentsYes))
-# telco<-subset(telco, select=-c(DeviceProtectionYes))
-# telco<-subset(telco, select=-c(PaymentMethodBank.transfer..automatic.))
-# telco<-subset(telco, select=-c(InternetServiceDSL))
-# telco<-subset(telco, select=-c(OnlineSecurityYes))
-# telco<-subset(telco, select=-c(OnlineBackupYes))
-# telco<-subset(telco, select=-c(StreamingTVYes))
-# telco<-subset(telco, select=-c(StreamingMoviesYes))
-# telco<-subset(telco, select=-c(ContractTwo.year))
-# telco<-subset(telco, select=-c(PaymentMethodCredit.card..automatic.))
-# telco<-subset(telco, select=-c(PaymentMethodMailed.check))
+###############################################################################
 
+# Prints fields which are correlated based on threshold
 getCorrelation(telco)
-
-# random <- cor.test(telco$tenure, telco$tenure, method = "pearson")
-# print(random$estimate)
-
 
 # Exporting the data frame
 write.csv(telco, "PreprocessedTelco.csv")
+}
+
+# ************************************************
+# Call our PreProcessing() function
+PreProcessing()
